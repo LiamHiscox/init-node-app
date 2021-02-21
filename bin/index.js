3da3f5b0d5f2e31b3e437241349b6b6e00afaccb
Liam Hiscox <liamhiscox@gmx.at>
2021-02-21 16:04:23 +0100

    split index.js and added 'new' command

--- bin/index.js
+++ bin/index.js
@@ -20,78 +20,22 @@ var __importStar = (this && this.__importStar) || function (mod) {
     return result;
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-var git_handler_1 = require("../lib/application/git-handler");
-var npm_handler_1 = require("../lib/application/npm-handler");
-var typescript_handler_1 = require("../lib/application/typescript-handler");
-var html_handler_1 = require("../lib/application/html-handler");
-var docker_handler_1 = require("../lib/application/docker-handler");
-var file_handler_1 = require("../lib/tools/file-handler");
 var yargs = __importStar(require("yargs"));
-var package_config_1 = require("../lib/tools/package-config");
+var new_1 = require("../lib/new");
 // TODO: add passing of list of npm packages to install (plus @types if tsc set to true)
-var options = yargs
+yargs
     .scriptName('init')
-    .usage('init <root>')
-    .command('<root>', 'initialize a new nodejs project in the root folder', function (yargs) {
-    yargs.positional('root', { describe: 'root folder of the project', type: 'string' });
+    .command('new <root>', 'initialize a new nodejs project in the root folder', function (yargs) {
+    yargs
+        .positional('root', { describe: 'root folder of the project', type: 'string' })
+        .option('n', { alias: 'npm', describe: 'Initializes NPM configuration', type: 'boolean' })
+        .option('g', { alias: 'git', describe: 'Initializes empty git repository', type: 'boolean' })
+        .option('t', { alias: 'tsc', describe: 'Initializes typescript configuration', type: 'boolean' })
+        .option('d', { alias: 'docker', describe: 'Adds a docker-compose file', type: 'boolean' })
+        .option('j', { alias: 'jest', describe: 'Adds jest for testing', type: 'boolean' })
+        .option('html', { describe: 'Initializes HTML project', type: 'boolean' });
+}, function (options) {
+    new_1.initNew(options);
 })
     .demandCommand(1, 1)
-    .option('n', { alias: 'npm', describe: 'Initializes NPM configuration', type: 'boolean' })
-    .option('g', { alias: 'git', describe: 'Initializes empty git repository', type: 'boolean' })
-    .option('t', { alias: 'tsc', describe: 'Initializes typescript configuration', type: 'boolean' })
-    .option('d', { alias: 'docker', describe: 'Adds a docker-compose file', type: 'boolean' })
-    .option('j', { alias: 'jest', describe: 'Adds jest for testing', type: 'boolean' })
-    .option('html', { describe: 'Initializes HTML project', type: 'boolean' })
     .argv;
-// TODO: validation
-var projectName = options._[0].toString();
-file_handler_1.FileHandler.createDir(projectName);
-process.chdir(projectName);
-// TODO: move to separate file (init.js)
-var SRC_PATH = 'src';
-var WEB_PATH = 'web';
-file_handler_1.FileHandler.createDir(SRC_PATH);
-if (options.npm) {
-    package_config_1.PackageConfig.setMain('index.js', SRC_PATH);
-    npm_handler_1.NpmHandler.initialize();
-}
-if (options.git) {
-    git_handler_1.GitHandler.initialize();
-    git_handler_1.GitHandler.gitIgnore();
-}
-if (options.tsc) {
-    typescript_handler_1.TypescriptHandler.createIndexFile(SRC_PATH);
-    typescript_handler_1.TypescriptHandler.install();
-    typescript_handler_1.TypescriptHandler.initialize(SRC_PATH);
-    typescript_handler_1.TypescriptHandler.addScripts(SRC_PATH);
-}
-else {
-    npm_handler_1.NpmHandler.createIndexFile(SRC_PATH);
-}
-if (options.docker) {
-    docker_handler_1.DockerHandler.initialize();
-}
-if (options.html && options.tsc) {
-    html_handler_1.HtmlHandler.initialize(WEB_PATH);
-    html_handler_1.HtmlHandler.createScriptFile(WEB_PATH, 'ts');
-    typescript_handler_1.TypescriptHandler.initialize(WEB_PATH, true);
-    typescript_handler_1.TypescriptHandler.addScripts(WEB_PATH);
-}
-if (options.html && !options.tsc) {
-    html_handler_1.HtmlHandler.initialize(WEB_PATH);
-    html_handler_1.HtmlHandler.createScriptFile(WEB_PATH);
-}
-if (options.jest && !options.tsc) {
-    npm_handler_1.NpmHandler.installPackage('jest');
-    package_config_1.PackageConfig.addScripts({ 'test': 'jest' });
-}
-if (options.jest && options.tsc) {
-    npm_handler_1.NpmHandler.installPackage('jest');
-    npm_handler_1.NpmHandler.installPackage('@types/jest');
-    var testScript = package_config_1.PackageConfig
-        .filterScriptKeys('build')
-        .map(function (script) { return "npm run " + script; })
-        .concat('jest')
-        .join(' && ');
-    package_config_1.PackageConfig.addScripts({ 'test': testScript });
-}
--- bin/index.ts
+++ bin/index.ts
@@ -1,84 +1,31 @@
 #!/usr/bin/env node
 
-import {GitHandler} from '../lib/application/git-handler';
-import {NpmHandler} from '../lib/application/npm-handler';
-import {TypescriptHandler} from '../lib/application/typescript-handler';
-import {HtmlHandler} from '../lib/application/html-handler';
-import {DockerHandler} from '../lib/application/docker-handler';
-import {FileHandler} from '../lib/tools/file-handler';
 import * as yargs from 'yargs';
-import {PackageConfig} from '../lib/tools/package-config';
+import {initNew, InitNewModel} from "../lib/new";
+import {Arguments} from "yargs";
 
 // TODO: add passing of list of npm packages to install (plus @types if tsc set to true)
-const options = yargs
+yargs
     .scriptName('init')
-    .usage('init <root>')
-    .command('<root>','initialize a new nodejs project in the root folder', (yargs) => {
-        yargs.positional('root', {describe: 'root folder of the project', type: 'string'})
-    })
+    .command(
+        'new <root>',
+        'initialize a new nodejs project in the root folder',
+        (yargs) => {
+            yargs
+                .positional('root', {describe: 'root folder of the project', type: 'string'})
+                .option('n', {alias: 'npm', describe: 'Initializes NPM configuration', type: 'boolean', default: false})
+                .option('g', {alias: 'git', describe: 'Initializes empty git repository', type: 'boolean', default: false})
+                .option('t', {alias: 'tsc', describe: 'Initializes typescript configuration', type: 'boolean', default: false})
+                .option('d', {alias: 'docker', describe: 'Adds a docker-compose file', type: 'boolean', default: false})
+                .option('j', {alias: 'jest', describe: 'Adds jest for testing', type: 'boolean', default: false})
+                .option('html', {describe: 'Initializes HTML project', type: 'boolean', default: false})
+        },
+        (options: Arguments<InitNewModel>) => {
+            if (options.root && options.root.trim()) {
+                initNew(options)
+            } else {
+                console.error('invalid project name given')
+            }
+        })
     .demandCommand(1, 1)
-    .option('n', {alias: 'npm', describe: 'Initializes NPM configuration', type: 'boolean'})
-    .option('g', {alias: 'git', describe: 'Initializes empty git repository', type: 'boolean'})
-    .option('t', {alias: 'tsc', describe: 'Initializes typescript configuration', type: 'boolean'})
-    .option('d', {alias: 'docker', describe: 'Adds a docker-compose file', type: 'boolean'})
-    .option('j', {alias: 'jest', describe: 'Adds jest for testing', type: 'boolean'})
-    .option('html', {describe: 'Initializes HTML project', type: 'boolean'})
     .argv;
-
-
-// TODO: validation
-const projectName = options._[0].toString();
-
-FileHandler.createDir(projectName);
-process.chdir(projectName);
-
-// TODO: move to separate file (init.js)
-const SRC_PATH = 'src';
-const WEB_PATH = 'web';
-
-FileHandler.createDir(SRC_PATH);
-
-if (options.npm) {
-    // add global config file for constants
-    PackageConfig.setMain('index.js', SRC_PATH)
-    NpmHandler.initialize();
-}
-if (options.git) {
-    GitHandler.initialize();
-    GitHandler.gitIgnore();
-}
-if (options.tsc) {
-    TypescriptHandler.createIndexFile(SRC_PATH);
-    TypescriptHandler.install();
-    TypescriptHandler.initialize(SRC_PATH);
-    TypescriptHandler.addScripts(SRC_PATH);
-} else {
-    NpmHandler.createIndexFile(SRC_PATH);
-}
-if (options.docker) {
-    DockerHandler.initialize();
-}
-if (options.html && options.tsc) {
-    HtmlHandler.initialize(WEB_PATH);
-    HtmlHandler.createScriptFile(WEB_PATH, 'ts');
-    TypescriptHandler.initialize(WEB_PATH, true);
-    TypescriptHandler.addScripts(WEB_PATH);
-}
-if (options.html && !options.tsc) {
-    HtmlHandler.initialize(WEB_PATH);
-    HtmlHandler.createScriptFile(WEB_PATH);
-}
-if (options.jest && !options.tsc) {
-    NpmHandler.installPackage('jest');
-    PackageConfig.addScripts({'test': 'jest'});
-}
-if (options.jest && options.tsc) {
-    NpmHandler.installPackage('jest');
-    NpmHandler.installPackage('@types/jest');
-    const testScript = PackageConfig
-        .filterScriptKeys('build')
-        .map(script => `npm run ${script}`)
-        .concat('jest')
-        .join(' && ');
-    PackageConfig.addScripts({'test': testScript});
-}
