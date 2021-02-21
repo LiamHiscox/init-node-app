3f19b10be424f1f0c94159c093a4b32cbc3de332
Liam Hiscox <liamhiscox@gmx.at>
2021-02-21 19:38:44 +0100

    split index.js and added 'new' command

--- bin/index.js
+++ bin/index.js
@@ -1,41 +1,35 @@
 #!/usr/bin/env node
 "use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
-    __setModuleDefault(result, mod);
-    return result;
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-var yargs = __importStar(require("yargs"));
-var new_1 = require("../lib/new");
+var newInit_1 = require("../lib/newInit");
+var yargs_1 = __importDefault(require("yargs"));
 // TODO: add passing of list of npm packages to install (plus @types if tsc set to true)
-yargs
+yargs_1.default
     .scriptName('init')
     .command('new <root>', 'initialize a new nodejs project in the root folder', function (yargs) {
     yargs
         .positional('root', { describe: 'root folder of the project', type: 'string' })
-        .option('n', { alias: 'npm', describe: 'Initializes NPM configuration', type: 'boolean' })
-        .option('g', { alias: 'git', describe: 'Initializes empty git repository', type: 'boolean' })
-        .option('t', { alias: 'tsc', describe: 'Initializes typescript configuration', type: 'boolean' })
-        .option('d', { alias: 'docker', describe: 'Adds a docker-compose file', type: 'boolean' })
-        .option('j', { alias: 'jest', describe: 'Adds jest for testing', type: 'boolean' })
-        .option('html', { describe: 'Initializes HTML project', type: 'boolean' });
+        .check(function (argv, options) {
+        console.log('options', options);
+        console.log('argv', argv);
+        return true;
+    })
+        .option('n', { alias: 'npm', describe: 'Initializes NPM configuration', type: 'boolean', default: false })
+        .option('g', { alias: 'git', describe: 'Initializes empty git repository', type: 'boolean', default: false })
+        .option('t', { alias: 'tsc', describe: 'Initializes typescript configuration', type: 'boolean', default: false })
+        .option('d', { alias: 'docker', describe: 'Adds a docker-compose file', type: 'boolean', default: false })
+        .option('j', { alias: 'jest', describe: 'Adds jest for testing', type: 'boolean', default: false })
+        .option('html', { describe: 'Initializes HTML project', type: 'boolean', default: false });
 }, function (options) {
-    new_1.initNew(options);
+    if (options.root && options.root.trim()) {
+        newInit_1.initNew(options);
+    }
+    else {
+        console.error('invalid project name given');
+    }
 })
     .demandCommand(1, 1)
     .argv;
--- bin/index.ts
+++ bin/index.ts
@@ -1,8 +1,7 @@
 #!/usr/bin/env node
 
-import * as yargs from 'yargs';
-import {initNew, InitNewModel} from "../lib/new";
-import {Arguments} from "yargs";
+import {initNew, InitNewModel} from "../lib/newInit";
+import yargs, {Arguments} from 'yargs';
 
 // TODO: add passing of list of npm packages to install (plus @types if tsc set to true)
 yargs
@@ -13,6 +12,13 @@ yargs
         (yargs) => {
             yargs
                 .positional('root', {describe: 'root folder of the project', type: 'string'})
+                .check(({ root }) => {
+                    if (root && root.trim()) {
+                        return true;
+                    } else {
+                        throw new Error('Please Provide a valid name for the project');
+                    }
+                })
                 .option('n', {alias: 'npm', describe: 'Initializes NPM configuration', type: 'boolean', default: false})
                 .option('g', {alias: 'git', describe: 'Initializes empty git repository', type: 'boolean', default: false})
                 .option('t', {alias: 'tsc', describe: 'Initializes typescript configuration', type: 'boolean', default: false})
@@ -21,11 +27,7 @@ yargs
                 .option('html', {describe: 'Initializes HTML project', type: 'boolean', default: false})
         },
         (options: Arguments<InitNewModel>) => {
-            if (options.root && options.root.trim()) {
-                initNew(options)
-            } else {
-                console.error('invalid project name given')
-            }
+            initNew({ ...options, root: options.root.trim() })
         })
     .demandCommand(1, 1)
     .argv;
